f8ee24397e6ef6ab18bd8160770f89f8facdb23a
hong9Lee <hongtemp9mac@gmail.com>
2024-01-01 20:09:08 +0900

    filter 추가

--- api/src/main/java/com/example/api/controller/TestController.java
+++ api/src/main/java/com/example/api/controller/TestController.java
@@ -1,13 +1,17 @@
 package com.example.api.controller;
 
-import org.springframework.stereotype.Controller;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@Slf4j
 public class TestController {
 
     @GetMapping("/api/test")
-    public void test() {
+    public String test() {
+        log.info("test controller init");
 
+        return "OK";
     }
 }
---
+++ gateway/src/main/java/com/example/gateway/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,31 @@
+package com.example.gateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class JwtAuthenticationFilter extends AbstractGatewayFilterFactory<JwtAuthenticationFilter.Config> {
+
+    // 필터 설정 클래스 (여기서는 빈 클래스)
+    public static class Config {
+        // 필터 설정에 필요한 필드를 여기에 추가 (현재는 비워둠)
+    }
+
+    public JwtAuthenticationFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            // 요청이 들어올 때 로그 출력
+            log.info("JwtAuthenticationFilter is applied to request: {}", exchange.getRequest().getPath());
+
+            // 다음 필터 또는 라우트 핸들러로 요청 전달
+            return chain.filter(exchange);
+        };
+    }
+}
