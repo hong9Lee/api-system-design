2280eeea7995710c5a5cf8115418cac717fb2898
hong9Lee <hongtemp9mac@gmail.com>
2024-01-11 22:17:03 +0900

    GatewayInterceptor 주석 추가

--- api/src/main/java/com/example/api/interceptor/GatewayInterceptor.java
+++ api/src/main/java/com/example/api/interceptor/GatewayInterceptor.java
@@ -7,6 +7,11 @@ import org.springframework.web.servlet.HandlerInterceptor;
 /**
  * Gateway를 통한 요청만 허용한다.
  * 요청 Header에 X-Gateway-Auth 검사.
+
+ 추가적으로 아래 방법으로도 구현 가능할 것 같다.
+ - IP 화이트 리스팅
+ - 로드 밸런서를 통해 Api 요청을 Gateway로 전부 라우팅
+ - Spring Cloud Gateway의 라우팅 기능 사용해보기.
  */
 public class GatewayInterceptor implements HandlerInterceptor {
 
