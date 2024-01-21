c93a2e01d97a2eea66d9ae35efa4fb65dc462f84
hong9Lee <hongtemp9mac@gmail.com>
2024-01-21 17:46:23 +0900

    websocket 모듈 추가

---
+++ web-websocket/src/main/java/com/example/webwebsocket/WebWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.webwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebWebsocketApplication.class, args);
+    }
+
+}
