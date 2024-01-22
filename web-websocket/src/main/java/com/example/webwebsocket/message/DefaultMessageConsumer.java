c7e2ab99b5ca74e2d1131ec0831cf5be95827843
hong9Lee <hongtemp9mac@gmail.com>
2024-01-22 17:48:54 +0900

    DefaultMessageConsumer 추가

---
+++ web-websocket/src/main/java/com/example/webwebsocket/message/DefaultMessageConsumer.java
@@ -0,0 +1,15 @@
+package com.example.webwebsocket.message;
+
+import com.example.shared.message.DefaultMessage;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import java.util.function.Consumer;
+
+@Slf4j
+@Component("defaultBroadcastMessageConsumer")
+public class DefaultMessageConsumer implements Consumer<DefaultMessage> {
+    @Override
+    public void accept(DefaultMessage defaultMessage) {
+        log.info("DefaultMessageConsumer uuid:{}, type:{}", defaultMessage.getUuid(), defaultMessage.getMessageType());
+    }
+}
