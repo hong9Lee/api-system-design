bf69fcbf4b6bfd143c0d63957826588fd1930028
hong9Lee <hongtemp9mac@gmail.com>
2024-01-28 18:19:27 +0900

    kafka consumer worker로 이동

---
+++ web-worker/src/main/java/com/example/worker/config/KafkaConfig.java
@@ -0,0 +1,42 @@
+package com.example.worker.config;
+
+import com.fasterxml.jackson.databind.deser.std.StringDeserializer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+public class KafkaConfig {
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactoryGroup1() {
+        return kafkaListenerContainerFactory("defaultGroup");
+    }
+
+    private ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(String groupId) {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory(groupId));
+        return factory;
+    }
+
+    private DefaultKafkaConsumerFactory<String, String> consumerFactory(String groupId) {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers); // Kafka 클러스터에 연결하기 위한 서버 목록
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId); // 컨슈머 그룹은 메시지를 공유하는 컨슈머의 집합으로, Kafka는 컨슈머 그룹 내의 컨슈머들에게 토픽의 파티션을 자동으로 분배해준다.
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class); // Kafka 메시지의 키를 역직렬화하는 데 사용되는 클래스
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class); // Kafka 메시지의 값(페이로드)를 역직렬화하는 데 사용되는 클래스
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest"); // 가장 초기의 오프셋부터 시작하여 모든 메시지를 처리하겠다
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+}
--- web-worker/src/main/java/com/example/worker/service/redis/KafkaConsumer.java
+++ web-worker/src/main/java/com/example/worker/service/redis/KafkaConsumer.java
@@ -1,4 +1,4 @@
-package com.example.api.service;
+package com.example.worker.service.redis;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -10,6 +10,6 @@ public class KafkaConsumer {
 
     @KafkaListener(topics = "defaultTopic", groupId = "defaultGroup")
     public void listen(String message) {
-        log.info("Received message in group defaultGroup:{}", message);
+        log.info("Received Kafka message in group defaultGroup:{}", message);
     }
 }
