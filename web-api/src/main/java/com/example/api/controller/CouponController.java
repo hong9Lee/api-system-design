833e93bf539ab3d7465c2ee8564419929aac6f9a
hong9Lee <hongtemp9mac@gmail.com>
2024-02-01 21:49:19 +0900

    jvm 설정 변경

--- web-api/src/main/java/com/example/api/controller/CouponController.java
+++ web-api/src/main/java/com/example/api/controller/CouponController.java
@@ -4,6 +4,7 @@ import com.example.api.model.CouponIssueVO;
 import com.example.api.service.CouponService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,9 +17,9 @@ public class CouponController {
     private final CouponService couponService;
 
     @PostMapping("/coupon/issue")
-    public String couponIssue(@RequestBody CouponIssueVO couponIssueVO) {
+    public ResponseEntity couponIssue(@RequestBody CouponIssueVO couponIssueVO) {
         log.info("CouponController couponIssue userId:{}", couponIssueVO.getUserId());
         couponService.issue(couponIssueVO);
-        return "OK";
+        return ResponseEntity.ok("OK");
     }
 }
