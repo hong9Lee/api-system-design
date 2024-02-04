54e9c8713f28d60b1604169b0bcadbdc6293c5e2
hong9Lee <hongtemp9mac@gmail.com>
2024-02-04 16:12:03 +0900

    web-eureka 모듈 추가

---
+++ web-eureka/src/test/java/com/example/webeureka/WebEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
