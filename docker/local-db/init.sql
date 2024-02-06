3f6c1df8680cb88db5b6275e7d8b320d2e87050a
hong9Lee <hongtemp9mac@gmail.com>
2024-02-06 22:28:17 +0900

    docker container간 통신 불가능으로 설정 변경

--- docker/local-db/init.sql
+++ docker/local-db/init.sql
@@ -1,5 +1,2 @@
 CREATE DATABASE local_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER 'hglee'@'localhost' IDENTIFIED BY 'hglee';
-CREATE USER 'hglee'@'%' IDENTIFIED BY 'hglee';
-GRANT ALL ON hglee.* to 'hglee'@'localhost';
-GRANT ALL ON hglee.* to 'hglee'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED WITH 'mysql_native_password' BY '102030';
