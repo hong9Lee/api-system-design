36bb5f7ce33e9acb7ae5a0d983dd12b42b00c68e
hong9Lee <hongtemp9mac@gmail.com>
2024-02-08 22:14:35 +0900

    discovery client 등록 성공

--- web-api/src/main/java/com/example/api/ApiApplication.java
+++ web-api/src/main/java/com/example/api/ApiApplication.java
@@ -3,7 +3,7 @@ package com.example.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -13,7 +13,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @ComponentScan(basePackages = {"com.example", "data"})
 @EntityScan(basePackages = {"data.entity"})
 @EnableJpaRepositories("data.repository")
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class ApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
